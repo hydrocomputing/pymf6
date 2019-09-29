f79b5383e3006e592d7b28a31ee2f865e476b835
Mike Mueller <mmueller@python-academy.de>
2019-09-29 15:36:06 +0200

    Bump up version

--- pymf6/__init__.py
+++ pymf6/__init__.py
@@ -4,7 +4,7 @@ Python Wrapper for MODFLOW 6
 
 __all__ = ['__version__', '__name__', '__author__']
 
-__version__ = '0.4.0'
+__version__ = '0.4.2'
 __name__ = 'pymf6'  # pylint: disable=redefined-builtin
 __author__ = 'Mike Müller'
 
