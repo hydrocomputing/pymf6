0ccf317cb98882fcc217cedc9bc02cb069d3f8e0
Mike Mueller <mmueller@python-academy.de>
2020-08-13 16:44:43 +0200

    Add `run` function that uses `multiprocessing` to avoid process termination
    MF6 calls `stop` and terminates the process. Running it an extra process
    prevents this.

--- pymf6/__init__.py
+++ pymf6/__init__.py
@@ -2,6 +2,10 @@
 Python Wrapper for MODFLOW 6
 """
 
+from multiprocessing import Process
+
+from . mf6 import mf6_sub  # pylint: disable-msg=no-name-in-module
+
 __all__ = ['__version__', '__name__', '__author__']
 
 __version__ = '0.4.2'
@@ -11,3 +15,17 @@ __author__ = 'Mike Müller'
 MFSIM_NAM = 'mfsim.nam'
 BACKUP_DIR = '__pymf6__backup'
 DATA_MAPPING = 'data_mapping.pkl'
+
+
+def run(callback):
+    """Run the MF6 model in the current paths with `callback`
+    """
+
+    def func():
+        """Helper function for run with `multiprocessing`
+        """
+        mf6_sub(callback)
+
+    process = Process(target=func)
+    process.start()
+    process.join()
