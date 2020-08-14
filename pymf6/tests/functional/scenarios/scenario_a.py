864febd6d9c1b4dab43586f39b32b694274c7dea
Mike Mueller <mmueller@python-academy.de>
2020-08-14 17:00:04 +0200

    Add test scenario builder and first scenario

---
+++ pymf6/tests/functional/scenarios/scenario_a.py
@@ -0,0 +1,50 @@
+"""Test sceanrio A (see Excel file `'ueflow_MF6_benchmarks.xlsx)
+"""
+
+from data.base_data_a import data as base_data
+from pymf6.callback import Func
+from pymf6.tests.functional.test_builder.runners import (
+    mf6_pure, mf6_pymf6, show_diff)
+
+
+class MyFunc(Func):
+    """Class whose instances act like a function, i.e. are callables
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.model = self.simulation.models[0]
+        self.sim = self.simulation.solution_groups[0]
+        self.chd_changed = False
+
+    def __call__(self):
+        """
+        Change `CHD` for stress period 2.
+        """
+        super().__call__()
+        # pylint: disable-msg=no-member
+        if not self.chd_changed and self.simulation.TDIS.KPER.value == 2:
+            self.chd_changed = True
+            self.model.CHD_0.BOUND[0][0][::2] = 2
+            self.model.CHD_0.BOUND[0][0][1::2] = 5
+
+
+def main():
+    """Run all models
+    """
+    mf6_pure('a_base', base_data=base_data)
+    data = {'chd': {
+                'stress_periods': [
+                    {'h_west': 6, 'h_east': 3},
+                    {'h_west': 2, 'h_east': 5},
+                    ]
+                }
+            }
+    mf6_pure(model_name='a_mf6_pure', base_data=base_data, data=data)
+    mf6_pymf6(model_name='a_pymf6', data=base_data, cb_cls=MyFunc)
+    show_diff('a_mf6_pure', 'a_base')
+    show_diff('a_mf6_pure', 'a_pymf6')
+
+
+if __name__ == '__main__':
+    main()
---
+++ pymf6/tests/functional/test_builder/runners.py
@@ -0,0 +1,51 @@
+"""Scenario runners
+"""
+
+from copy import deepcopy
+
+import flopy
+
+from pymf6.tools.tempdir import TempDir
+from pymf6.tests.functional.test_builder.base_model import BaseModel
+from pymf6 import run
+
+
+def mf6_pure(model_name, base_data, data=None):
+    """Run an M6 model with `flopy``
+
+`base_data` and `data` will be merged
+    """
+    model_data = deepcopy(base_data)
+    if data:
+        model_data.update(data)
+    model = BaseModel(model_name, data=model_data)
+    model.write_simulation()
+    model.run_simulation()
+
+
+def mf6_pymf6(model_name, cb_cls, data, sim_dir='.simulations'):
+    """Run a MF6 mode wot `pymf6` using `cb_cls`
+    """
+    model = BaseModel(model_name, data)
+    model.write_simulation()
+    with TempDir(f'{sim_dir}/{model_name}'):
+        run(cb_cls())
+
+
+def read_head(model_name, head_file=None, sim_dir='.simulations'):
+    """Read head file of given run
+    """
+    if head_file is None:
+        head_file = f'{sim_dir}/{model_name}/{model_name}.hds'
+    return flopy.utils.HeadFile(head_file).get_data()
+
+
+def show_diff(name1, name2):
+    """Show maximun and minimum head difference between two models
+    """
+    diff = read_head(name1) - read_head(name2)
+    header = f'Head Difference between scenario "{name1}" and "{name2}"'
+    print(header)
+    print('=' * len(header))
+    print(f'Min diff: {diff.min()}, Max diff: {diff.max()}')
+    print()
