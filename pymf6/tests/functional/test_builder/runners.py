b6b789c4b7aafd150e10d99a11037769031f535a
Mike Mueller <mmueller@python-academy.de>
2020-09-19 20:58:23 +0200

    Add parameter sweeper function

--- pymf6/tests/functional/test_builder/runners.py
+++ pymf6/tests/functional/test_builder/runners.py
@@ -2,13 +2,17 @@
 """
 
 from copy import deepcopy
+import os
+import shelve
 
 import flopy
+from frozendict import frozendict
 import numpy as np
 
 from pymf6.tools.tempdir import TempDir
 from pymf6.tests.functional.test_builder.base_model import BaseModel
 from pymf6 import run
+
 
 
 def mf6_pure(model_name, base_data, data=None):
@@ -18,7 +22,8 @@ def mf6_pure(model_name, base_data, data=None):
     """
     model_data = deepcopy(base_data)
     if data:
-        model_data.update(data)
+        for key, value in data.items():
+            model_data[key].update(value)
     model = BaseModel(model_name, data=model_data)
     model.write_simulation()
     model.run_simulation()
@@ -74,3 +79,31 @@ def calc_errors(target_name, prediction_name):
         'RMSE': rmse(predictions, targets),
         'MSE': mse(predictions, targets)
     }
+
+
+def run_parameter_sweep(
+        key,
+        CallbackFunc,
+        data,
+        new_base_data=None,
+        base_data=None,
+        scenario_name='a'):
+    """Run MF6 and pymf6 models with same parameters.
+
+    Results are saved in shelve database.
+    """
+    model_name_mf6 = f'{scenario_name}_mf6_param'
+    model_name_pymf6 = f'{scenario_name}_pymf6_param'
+    if new_base_data:
+        for dkey, value in new_base_data.items():
+            base_data[dkey].update(value)
+    mf6_pure(model_name=model_name_mf6, base_data=base_data, data=data)
+    mf6_pymf6(model_name=model_name_pymf6, data=base_data, cb_cls=CallbackFunc,
+              kwargs=frozendict({'data': data}))
+    res = {}
+    res['errors'] = calc_errors(model_name_pymf6, model_name_mf6)
+    res['data'] = data
+    res['new_base_data'] = new_base_data
+    flag = 'a' if os.path.exists(f'{scenario_name}_param.db.dat') else 'c'
+    with shelve.open(f'{scenario_name}_param.db', flag) as db:
+        db[key] = res
