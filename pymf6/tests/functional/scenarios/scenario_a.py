ece0b356a2303df9ea17615857f2904e9d716b7d
Mike Mueller <mmueller@python-academy.de>
2020-09-20 16:32:35 +0200

    Add multiple runs for different parameters

--- pymf6/tests/functional/scenarios/scenario_a.py
+++ pymf6/tests/functional/scenarios/scenario_a.py
@@ -5,10 +5,12 @@ from pprint import pprint
 
 from frozendict import frozendict
 
-from data.base_data_a import data as base_data_a
 from pymf6.callback import Func
 from pymf6.tests.functional.test_builder.runners import (
-    mf6_pure, mf6_pymf6, show_diff, calc_errors, run_parameter_sweep)
+    mf6_pure, mf6_pymf6, show_diff, calc_errors, run_parameter_sweep,
+    make_error_stats)
+
+from data.base_data_a import data as base_data_a
 
 
 class MyFunc(Func):
@@ -30,12 +32,14 @@ class MyFunc(Func):
         # pylint: disable-msg=no-member
         if not self.chd_changed and self.simulation.TDIS.KPER.value == 2:
             self.chd_changed = True
-            self.model.CHD_0.BOUND[0][0][::2] = self.chd['stress_periods'][1]['h_west']
-            self.model.CHD_0.BOUND[0][0][1::2] = self.chd['stress_periods'][1]['h_east']
+            chd = self.chd['stress_periods'][1]
+            self.model.CHD_0.BOUND[0][0][::2] = chd['h_west']
+            self.model.CHD_0.BOUND[0][0][1::2] = chd['h_east']
 
 
-class Empty:
-    pass
+class Empty:  # pylint: disable-msg=too-few-public-methods
+    """Do nothing
+    """
 
 
 def run_base():
@@ -49,11 +53,10 @@ def run_base():
                     ]
                 }
             }
-    print(frozendict(data))
     mf6_pure('a_base', base_data=base_data_a)
     mf6_pure(model_name='a_mf6_pure', base_data=base_data_a, data=data)
     mf6_pymf6(model_name='a_pymf6', data=base_data_a, cb_cls=MyFunc,
-               kwargs={'data': frozendict(data)})
+              kwargs={'data': frozendict(data)})
     mf6_pymf6(model_name='a_pymf6_base', data=base_data_a, cb_cls=Empty)
 
     show_diff('a_base', 'a_mf6_pure')
@@ -62,7 +65,6 @@ def run_base():
     show_diff('a_base', 'a_pymf6_base')
     pprint(calc_errors('a_base', 'a_pymf6_base'))
     show_diff('a_base', 'a_pymf6')
-
 
 
 def run_scenario_a(key='base', new_base_data=None):
@@ -75,20 +77,56 @@ def run_scenario_a(key='base', new_base_data=None):
                     ]
                 }
             }
-    run_parameter_sweep(key, MyFunc, data, base_data=base_data_a, new_base_data=new_base_data)
+    run_parameter_sweep(
+        key, MyFunc, data, base_data=base_data_a, new_base_data=new_base_data)
+
+
+def run_domain():
+    """Run calculations with different domain sizes
+    """
+    tops = [
+                (0, -10, -20),
+                (-20, -30, -40),
+                (-10, -20, -30)
+            ]
+    for x in [100, 3000, 5000]:
+        for y in [100, 1000, 5000]:
+            for top, upper_bot, lower_bot in tops:
+                run_scenario_a(
+                    key=f'x{x}_y{y}_top_{top}_upper_bot{upper_bot}_lower_bot{lower_bot}',
+                    new_base_data={
+                        'dis': {
+                            'len_x': x,
+                            'len_y': y,
+                            'top': top,
+                            'upper_bot': upper_bot,
+                            'lower_bot': lower_bot}
+                    },
+                )
 
 
 def run_all():
-    run_scenario_a()
-    run_scenario_a(
-        key='x100_y100',
-        new_base_data={'dis':{'len_x': 300,'len_y': 100}},
-    )
-    run_scenario_a(
-        key='x5000_y5000',
-        new_base_data={'dis':{'len_x': 5000,'len_y': 5000}},
-    )
+    """Run all calculations
+    """
+    run_domain()
+    # run_scenario_a()
+    # run_scenario_a(
+    #     key='x100_y100',
+    #     new_base_data={'dis':{'len_x': 300,'len_y': 100}},
+    # )
+    # run_scenario_a(
+    #     key='x5000_y5000',
+    #     new_base_data={'dis':{'len_x': 5000,'len_y': 5000}},
+    # )
+
+
+def show_errors():
+    """Show cumulated error statistics
+    """
+    pprint(make_error_stats('a'))
+
 
 if __name__ == '__main__':
     # run_base()
-    run_all()
+    # run_all()
+    show_errors()
