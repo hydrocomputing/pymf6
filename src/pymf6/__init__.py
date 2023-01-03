a60b7447550fa6f96e8a8afed6e7e015f8f60115
pya <mmueller@python-academy.de>
2023-01-03 14:23:28 +0100

    Add meta infos to package

--- src/pymf6/__init__.py
+++ src/pymf6/__init__.py
@@ -2,5 +2,18 @@
 High-level interface to MODFLOW 6.
 """
 
-from ._version import *
+try:
+    import xmipy
+except ImportError:
+    print('Please install xmipy:')
+    print('    pip install xmipy')
+from ._version import version as __version__
 from .mf6 import MF6
+
+
+path, ini = MF6.read_ini()
+__ini_path__ = str(path)
+__dll_path__ = ini['paths']['dll_path']
+__xmipy_version__ = xmipy.__version__
+
+del xmipy
