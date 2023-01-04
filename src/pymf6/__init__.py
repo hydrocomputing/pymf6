62c82b2d94e16905daa0fe8ba2783a45f81ab14b
pya <mmueller@python-academy.de>
2023-01-04 09:20:25 +0100

    Raise error if dll path does not exist

--- src/pymf6/__init__.py
+++ src/pymf6/__init__.py
@@ -2,6 +2,7 @@
 High-level interface to MODFLOW 6.
 """
 
+from pathlib import Path
 import sys
 
 try:
@@ -18,6 +19,11 @@ path, ini = MF6.read_ini()
 __ini_path__ = str(path) if path else path
 if ini:
     __dll_path__ = ini['paths']['dll_path']
+    if not Path(__dll_path__).exists():
+        raise ValueError(
+            f'dll path `{__dll_path__}` does not exist\n'
+            'Please specify correct path.'
+            )
 else:
     __dll_path__ = None
 __xmipy_version__ = xmipy.__version__
