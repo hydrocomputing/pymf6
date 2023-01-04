adde7c26601422d0bfc6cfd02078efbc88a57e49
pya <mmueller@python-academy.de>
2023-01-04 09:11:43 +0100

    Set paths to None if not found

--- src/pymf6/__init__.py
+++ src/pymf6/__init__.py
@@ -15,8 +15,11 @@ from .mf6 import MF6
 
 
 path, ini = MF6.read_ini()
-__ini_path__ = str(path)
-__dll_path__ = ini['paths']['dll_path']
+__ini_path__ = str(path) if path else path
+if ini:
+    __dll_path__ = ini['paths']['dll_path']
+else:
+    __dll_path__ = None
 __xmipy_version__ = xmipy.__version__
 
 del xmipy
