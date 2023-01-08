8fa30ed8748ad232d569592b7a409ca98b0d12d2
pya <mmueller@python-academy.de>
2023-01-08 12:14:18 +0100

    Change depricated config options; turn off unused

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'myst_nb',
 ]
 
-jupyter_execute_notebooks = "off"
+nb_execution_mode = "off"
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -70,7 +70,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 
