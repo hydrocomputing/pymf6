2f25dfff4f8d16b374c37048fb1aa74a9d7f29bc
Mike Müller <mmueller@python-academy.de>
2023-05-06 09:25:23 +0200

    Add two-well control

--- doc/examples/sys_injection_extraction/pymf6_sys_injection_extraction.py
+++ doc/examples/sys_injection_extraction/pymf6_sys_injection_extraction.py
@@ -1,28 +1,28 @@
 from pymf6.mf6 import MF6
 
 def run_model(nam_file):
-    mf6 = MF6(nam_file=nam_file, dll_path = r"C:/Users/Lucia Pedrosa/mf6.4.1/bin/libmf6.dll")
-    head = mf6.vars['SLN_1/X'] # solution one
-    wel_index_in = 11
-    wel_index_out = 88
-    obs = 55
+    mf6 = MF6(nam_file=nam_file)
+    head = mf6.vars['SLN_1/X']
+    wel_index_in = 22
+    wel_index_out = 66
     tolerance = 0.01
-    head_limit = 0.7
-    wel_in_pos = (0, 0)
-    wel_out_pos = (1, 0)
-    upper_limit = head_limit + tolerance
-    lower_limit = head_limit - tolerance
-    wel = mf6.vars['SYSINOUTWEL/WEL_0/BOUND'] # name variable regarding well
-    been_below = False
+    head_limit_out = 0.5
+    head_limit_in = 0.95
+    upper_limit_in = head_limit_in + tolerance
+    upper_limit_out = head_limit_out + tolerance
+    lower_limit_out = head_limit_out - tolerance
+    wel_out, wel_in = mf6.vars['SYSINOUTWEL/WEL_0/BOUND']
+    pumping_reduced = False
     for step in mf6.steps():
-        if step < 21:
-            if head[wel_index_out] <= upper_limit:
-                 wel[wel_out_pos] *= 0.9
-                 been_below = True
-            elif been_below and head[wel_index_out] >= upper_limit:
-                 wel[wel_out_pos] *= 1.1
-            wel[wel_in_pos] = -wel[wel_out_pos]
+        if step >= 11 and step < 21:
+            if ((head[wel_index_in] > upper_limit_in) or
+                (head[wel_index_out] < lower_limit_out)):
+                wel_out[0] = wel_out[0] * 0.9
+                pumping_reduced = True
+            elif pumping_reduced and head[wel_index_out] > upper_limit_out:
+                wel_out[0] = wel_out[0] * 1.1
+            wel_in[0] = -wel_out[0]
 
 
 if __name__ == '__main__':
-    run_model(r'models/mf6/mfsim.nam')
+    run_model(r'models/pymf6/mfsim.nam')
