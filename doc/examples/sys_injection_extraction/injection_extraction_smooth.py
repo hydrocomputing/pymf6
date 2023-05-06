eecded4ef75747d4cf311988f95eb37752a6d9da
Mike Müller <mmueller@python-academy.de>
2023-05-06 12:10:55 +0200

    Re-run all notebooks with new loop

--- doc/examples/sys_injection_extraction/injection_extraction_smooth.py
+++ doc/examples/sys_injection_extraction/injection_extraction_smooth.py
@@ -13,8 +13,15 @@ def run_model(nam_file):
     lower_limit_out = head_limit_out - tolerance
     wel_out, wel_in = mf6.vars['SYSINOUTWEL/WEL_0/BOUND']
     pumping_reduced = False
+    init_reduce = True
+    init_reduce_amount = 1
     for step in mf6.steps():
         if step >= 11 and step < 21:
+            if init_reduce and  step < 11.1:
+                wel_out[0] = wel_out[0] * 0.9
+                init_reduce_amount *= 0.9
+                if init_reduce_amount < 0.5:
+                    init_reduce = False
             if ((head[wel_index_in] > upper_limit_in) or
                 (head[wel_index_out] < lower_limit_out)):
                 wel_out[0] = wel_out[0] * 0.99
