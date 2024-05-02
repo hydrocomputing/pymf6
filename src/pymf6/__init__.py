f8525bef7da76975689db3dbe6bc8d278ae0d12a
Mike Müller <mmueller@python-academy.de>
2024-05-02 08:19:50 +0200

    Add strings for model prefixes

--- src/pymf6/__init__.py
+++ src/pymf6/__init__.py
@@ -1,6 +1,4 @@
-"""
-High-level interface to MODFLOW 6.
-"""
+"""High-level interface to MODFLOW 6."""
 
 from .tools.info import get_info_data
 
@@ -12,3 +10,8 @@ __xmipy_version__ = info['xmipy_version']
 __version__ = __pymf6_version__ = info['pymf6_version']
 __modflow_version__ = info.get('modflow_version')
 __mf6_exe__ = info.get('exe_path')
+__model_prefixes__ = {
+    'flow': 'gwf_',
+    'transport': 'gwt_',
+    'energy': 'gwe_',
+    }
