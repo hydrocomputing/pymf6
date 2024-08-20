f7de6f0b99c4913fe7e6fe82b990f55078b7be16
Mike Müller <mmueller@python-academy.de>
2024-08-20 12:29:19 +0200

    Add support for multiple stress periods

--- doc/examples/analytical/run_controlled.py
+++ doc/examples/analytical/run_controlled.py
@@ -19,7 +19,8 @@ def make_analytic_well_cells(wel, analytic_col_name='PYMF6_ANALYTIC'):
     """Create data for anayltic wells."""
     df = wel.stress_period_data.dataframe
     if wel.get_advanced_var('inamedbound')[0]:
-        df['wel_name'] = [name.lower() for name in wel.get_advanced_var('boundname_idm')]
+        bound_names = wel.get_advanced_var('boundname_idm')[:len(df)]
+        df['wel_name'] = [name.lower() for name in bound_names]
         if not df['wel_name'].is_unique:
             multiples = df[df.duplicated('wel_name')]['wel_name'].values
             indent = '\n' + ' ' * 12
@@ -36,25 +37,39 @@ def make_analytic_well_cells(wel, analytic_col_name='PYMF6_ANALYTIC'):
     return {name: {'index': index, 'q': q} for name, q, index in zip(df['wel_name'], df['q'], df.index)}
 
 
+def make_analytic_wells(gwf, analytic_well_data, kper):
+    """Create new analytic  well models."""
+    analytic_well_data = {} if analytic_well_data is None else analytic_well_data
+    wel = gwf.wel
+    analytic_well_cells = make_analytic_well_cells(wel)
+    for name in analytic_well_data:
+        if name not in analytic_well_cells:
+            raise ValueError(f'no MF6 data for {name} in stress period {kper + 1}')
+    well_levels = {name: [] for name in analytic_well_cells}
+    awells = {}
+    for name, well_cell_data in analytic_well_cells.items():
+        well_data = analytic_well_data.get(name)
+        awells[name] = AnalyticWell(
+            gwf, node_list_index=well_cell_data['index'], well_data=well_data)
+    return analytic_well_cells, well_levels, awells
+
+
 def run_model(model_path_controlled='models/c_100_100_multi_well', analytic_well_data=None):
     """Run a test model."""
     with warnings.catch_warnings(action='ignore'), open(devnull, 'w') as fobj:
         mf6 = MF6(model_path_controlled + '/mfsim.nam', use_modflow_api=True)
         gwf = mf6.models['gwf6']
-        wel = gwf.wel
-        analytic_well_cells = make_analytic_well_cells(wel)
-        well_levels = {name: [] for name in analytic_well_cells}
         old_totim = gwf.totim
+        kper = gwf.kper
+        res = {}
+        analytic_well_cells, well_levels, awells = make_analytic_wells(gwf, analytic_well_data.get(kper), kper)
         with redirect_stdout(fobj):
-            awells = {}
-            for name, well_cell_data in analytic_well_cells.items():
-                if analytic_well_data:
-                    well_data = analytic_well_data.get(name)
-                else:
-                    well_data = None
-                awells[name] = AnalyticWell(
-                    gwf, node_list_index=well_cell_data['index'], well_data=well_data)
             for model in mf6.model_loop():
+                if gwf.kper > kper:
+                    res[kper] = well_levels
+                    kper = gwf.kper
+                    analytic_well_cells, well_levels, awells = make_analytic_wells(gwf,
+                                                                                   analytic_well_data.get(kper), kper)
                 new_totim = model.totim
                 time_diff = new_totim - old_totim
                 if time_diff > 0:
@@ -63,4 +78,5 @@ def run_model(model_path_controlled='models/c_100_100_multi_well', analytic_well
                         awells[name].calc_well_head(-well_cell_data['q'], end_time=time_diff)
                         )
                     old_totim = new_totim
-    return well_levels
+            res[kper] = well_levels
+    return res
